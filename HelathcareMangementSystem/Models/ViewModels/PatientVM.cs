51fe2067543b0b0abc7a5571fe87b0edbe88d78f
Rakhi Ranglani <rakhi.ranglani222@outlook.com>
2021-04-21 01:53:32 +0100

    updated changes for health care information pages

---
+++ HelathcareMangementSystem/Models/ViewModels/PatientVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HealthcareMangementSystem.Models.ViewModels
+{
+    public class PatientVM
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string PatientName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string PatientLastName { get; set; }
+    }
+}
