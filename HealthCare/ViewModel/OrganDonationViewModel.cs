e21f9d3564d6725745226a5ebf6459b444f95391
Rakhi Ranglani <rakhi.ranglani222@outlook.com>
2021-04-27 23:28:53 +0100

    Updated changes  for organ  event details page.

---
+++ HealthCare/ViewModel/OrganDonationViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HealthCare.ViewModel
+{
+    [Keyless]
+    public class OrganDonationViewModel
+    {
+        public DateTime EventDate { get; set; }
+        public string EventDescription { get; set; }
+
+
+        public string Organ { get; set; }
+        public string AvailabilityInfo { get; set; }
+        public string HasOrganDonation { get; set; }
+
+        public string HospitalName { get; set; }
+
+        public string City { get; set; }
+
+
+        public string County { get; set; }
+
+        public string EIRCode { get; set; }
+
+        public List<OrganDonationViewModel> OrganDetailslist { get; set; }
+    }
+}
