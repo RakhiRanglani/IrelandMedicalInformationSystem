b34fc955a302b0956c48fae300096dcc20394430
sheldonj0hn <79276758+sheldonj0hn@users.noreply.github.com>
2021-04-28 21:16:27 +0100

    viewmodel

---
+++ HealthCare/ViewModel/RehabilitationViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HealthCare.ViewModel
+{
+    public class RehabilitationViewModel
+    {
+        public string RehabilitationType { get; set; }
+
+        public string CentreDetails { get; set; }
+
+        public string AvailabilityDetails { get; set; }
+
+        public string HospitalName { get; set; }
+
+        public string LocationID { get; set; }
+
+        public string City { get; set; }
+
+        public List<RehabilitationViewModel> Rehablist { get; set; }
+
+
+    }
+}
